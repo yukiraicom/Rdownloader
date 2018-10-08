5cf8b881baf186160d5f90db3a9245d34d11d8c6
yukiraicom <yuukiarai19970524@gmail.com>
2018-10-08 19:21:20 +0900

    delete Download class

--- lib/Rdownloader.rb
+++ lib/Rdownloader.rb
@@ -7,41 +7,39 @@ require 'ruby-progressbar'
 require 'securerandom'
 
 module Rdownloader
-  class Download
-    def self.download(path_origin)
-      a = URI.escape(path_origin)
-      uri = URI.parse(a)
-      path_parsed = uri
-      redirect_url = Net::HTTP.get_response(path_parsed)['location']
-      path = redirect_url #string
-      path = path_origin if path.nil?
-      filenamevar1 = "#{SecureRandom.hex(2)}" #適当なファイル名
-      filenamevar2 = ".pdf"
-      @@fileName = "#{filenamevar1}" + "#{filenamevar2}" #ここはスクレイピングする
-      new_url = URI.encode(path) #string
-      new_url_fileName = URI.decode(path).force_encoding('UTF-8')
-      content_length = nil
-      progress_bar = nil
-        open(@@fileName, 'wb') do |output|
-          p "ダウンロード中..."
-          open(new_url,
-            :content_length_proc => lambda{ |content_length| 
-              if content_length
-                # プログレスバーの最大長にcontent-lengthを指定
-                progress_bar = ProgressBar.create(:total => content_length)
-              end
-            },
-            :progress_proc => lambda{ |transferred_bytes|
-              if progress_bar
-                # プログレスバーの進捗状況にこれまで転送されたバイト数を代入する
-                progress_bar.progress = transferred_bytes
-              else
-                puts "#{transferred_bytes} / Total size is unknown"
-              end
-            }
-          ) do |data|
-            output.write(data.read)
-          end
+  def self.download(path_origin)
+    a = URI.escape(path_origin)
+    uri = URI.parse(a)
+    path_parsed = uri
+    redirect_url = Net::HTTP.get_response(path_parsed)['location']
+    path = redirect_url #string
+    path = path_origin if path.nil?
+    filenamevar1 = "#{SecureRandom.hex(2)}" #適当なファイル名
+    filenamevar2 = ".pdf"
+    @@fileName = "#{filenamevar1}" + "#{filenamevar2}" #ここはスクレイピングする
+    new_url = URI.encode(path) #string
+    new_url_fileName = URI.decode(path).force_encoding('UTF-8')
+    content_length = nil
+    progress_bar = nil
+      open(@@fileName, 'wb') do |output|
+        p "ダウンロード中..."
+        open(new_url,
+          :content_length_proc => lambda{ |content_length| 
+            if content_length
+              # プログレスバーの最大長にcontent-lengthを指定
+              progress_bar = ProgressBar.create(:total => content_length)
+            end
+          },
+          :progress_proc => lambda{ |transferred_bytes|
+            if progress_bar
+              # プログレスバーの進捗状況にこれまで転送されたバイト数を代入する
+              progress_bar.progress = transferred_bytes
+            else
+              puts "#{transferred_bytes} / Total size is unknown"
+            end
+          }
+        ) do |data|
+          output.write(data.read)
         end
     end
   end
